443c8c3c4cc45363a0cadc02a34f8c6f98f0f0f2
tabino87 <ysdck809@yahoo.co.jp>
2021-10-09 13:53:10 +0900

    usersテーブルの作成

--- db/migrate/20211007113936_devise_create_users.rb
+++ db/migrate/20211007113936_devise_create_users.rb
@@ -3,6 +3,13 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :nickname,               null: false
+      t.string :last_name,              null: false
+      t.string :first_name,             null: false
+      t.string :last_name_kana,         null: false
+      t.string :first_name_kana,        null: false
+      t.date :user_birth_date,          null: false
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
