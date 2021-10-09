f43558960b99da13413631cf8baf7845b37ee092
tabino87 <ysdck809@yahoo.co.jp>
2021-10-09 21:26:15 +0900

    deviseに関するビューファイルの配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def basic_auth
@@ -9,5 +12,9 @@ class ApplicationController < ActionController::Base
         username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
       end
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
