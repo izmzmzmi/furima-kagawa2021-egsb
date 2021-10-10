09bcb9ad1f841097c1e64b7507bcc21f18dfc412
tabino87 <ysdck809@yahoo.co.jp>
2021-10-10 23:33:14 +0900

    ユーザー認証機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
+  #before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
+
+  # #def move_to_index
+  #   #unless user_signed_in?
+  #     #redirect_to action: :index
+  #   end
+  # end
 end
