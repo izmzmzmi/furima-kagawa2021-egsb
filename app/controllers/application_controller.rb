21555361f4ef17825240c7a22cc8d2076c92e28d
tabino87 <ysdck809@yahoo.co.jp>
2021-10-10 21:33:44 +0900

    ユーザー情報表示の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_birth_date, :email])
   end
 end
