1c9c5c3733f73b75cc93a4b6d522c9f27b8972f8
izmzmzmi <cannedsababooks@gmail.com>
2021-10-14 02:41:01 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,20 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
   # validates :name, presence: true
   # validates :info, presence: true
-  # validates :category_id, presence: true
-  # validates :sales_status_id, presence: true
-  # validates :shipping_fee_status_id, presence: true
-  # validates :prefecture_id, presence: true
-  # validates :scheduled_delivery_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :sales_status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :scheduled_delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   # validates :price, presence: true
   # validates :image, presence: true
 end
