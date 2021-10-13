0aa7552f5e0cb529e33e8c09e90d25f7ee7a728a
izmzmzmi <cannedsababooks@gmail.com>
2021-10-13 21:28:32 +0900

    itemsルーティング、アクション、ビューの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,13 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+  
   # #def move_to_index
   #   #unless user_signed_in?
   #     #redirect_to action: :index
   #   end
   # end
+  
 end
