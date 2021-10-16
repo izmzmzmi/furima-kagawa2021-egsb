00b14197d1f0f393bbced01f7881cecafddeae4b
izmzmzmi <cannedsababooks@gmail.com>
2021-10-17 01:19:06 +0900

    ビューとコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :order
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(item_id: current_item.id, user_id: current_user.id)
+  end
+end
