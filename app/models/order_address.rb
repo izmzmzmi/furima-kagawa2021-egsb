9546f96a13b7294c66befb1537d46db3048075c9
izmzmzmi <cannedsababooks@gmail.com>
2021-10-22 01:02:15 +0900

    住所登録

---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}   
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
+    validates :item_id
+    validates :user_id
+    validates :token
+  end
+    validates :prefecture_id,  numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only:[:index, :new, :create, :show]
+  resources :items, only:[:index, :new, :create, :show] do
+    resources :orders, only:[:index, :new, :create]
+  end
   resources :users, only: :show
+  
 end
