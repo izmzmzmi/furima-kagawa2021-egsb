4a12c341f7faa333c4a26357184eda39f752a16b
tabino87 <ysdck809@yahoo.co.jp>
2021-10-21 13:38:50 +0900

    Rubocopによる変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
-   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -25,9 +25,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
   def update
@@ -40,11 +38,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   def show
-    @item =Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
+
   private
 
   def item_params
