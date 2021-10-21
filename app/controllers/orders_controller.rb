14945fb9375176f0c15fbc249853fdee129c95f7
izmzmzmi <cannedsababooks@gmail.com>
2021-10-22 01:03:10 +0900

    クレカ決済と環境変数設定（ローカル）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def new
@@ -7,18 +9,30 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      render :order
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(item_id: current_item.id, user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,10 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("@rails/activestorage").start()
+//require("@rails/activestorage").start()
 require("channels")
 require("../item")
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
