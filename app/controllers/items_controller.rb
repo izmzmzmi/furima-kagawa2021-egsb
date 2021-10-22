035a405e82301a2fa2b21a1c86f1cc34451bb694
tabino87 <ysdck809@yahoo.co.jp>
2021-10-22 23:37:54 +0900

    Rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-       redirect_to item_path(@item)
+      redirect_to item_path(@item)
     else
       render :edit
     end
