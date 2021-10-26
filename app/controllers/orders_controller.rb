1594bb0fce4bbd5ae409873ed8a90526e360e825
izmzmzmi <cannedsababooks@gmail.com>
2021-10-26 18:43:24 +0900

    共通の処理のまとめ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     if user_signed_in? && @item.user_id != current_user.id && !@item.order.present?
       @order_address = OrderAddress.new
     else
@@ -12,7 +13,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -38,5 +38,9 @@ class OrdersController < ApplicationController
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
